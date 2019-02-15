11c43993e2550dc07b4856f30a57379068252e06
ab7383 <378374573@qq.com>
2019-02-15 10:02:58 +0800

    利用Echarts画能力雷达图

--- angular-code/src/app/app.module.ts
+++ angular-code/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { HttpClientModule } from '@angular/common/http';
 // import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 // import { InMemoryDataService }  from './in-memory-data.service';
 import { HeroSearchComponent } from './hero-search/hero-search.component';
+import {NgxEchartsModule} from 'ngx-echarts';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { HeroSearchComponent } from './hero-search/hero-search.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    NgxEchartsModule
     // HttpClientInMemoryWebApiModule.forRoot(
     //   InMemoryDataService, { dataEncapsulation: false }
     //)
--- angular-code/src/app/hero-detail/hero-detail.component.ts
+++ angular-code/src/app/hero-detail/hero-detail.component.ts
@@ -13,20 +13,26 @@ import { HeroService } from '../hero.service';
 export class HeroDetailComponent implements OnInit {
 
   hero:Hero;
+  options : any;
+  map:{[key:string] : number} = {};
   constructor(
     private route : ActivatedRoute,
     private heroService : HeroService,
-    private location : Location
-  ) { }
+    private location : Location,
+  ) {
+    this.charToNumber();
+   }
 
   ngOnInit() {
-    this.getHero( );
+    this.getHero();
   }
 
   getHero():void
   {
     const id = + this.route.snapshot.paramMap.get('id');
-    this.heroService.getHero(id).subscribe(hero => this.hero = hero);
+    this.heroService.getHero(id).subscribe(hero =>{ 
+      this.radarChart(hero);
+      return this.hero = hero; });
   }
 
   goBack(): void {
@@ -37,4 +43,71 @@ export class HeroDetailComponent implements OnInit {
     this.heroService.updateHero(this.hero)
       .subscribe(() => this.goBack());
   }
+  
+  radarChart(hero : Hero):void{
+    this.options = {
+      title : {
+          text: hero.standName,
+          x : 'center',
+      },
+      tooltip : {
+          trigger: 'axis'
+      },
+      toolbox: {
+          show : false,
+          feature : {
+              mark : {show: true},
+              saveAsImage : {show: true}
+          }
+      },
+      calculable : true,
+      polar : [
+          {
+              indicator : [
+                  {text : '力量', max  : 100},
+                  {text : '速度', max  : 100},
+                  {text : '成长性', max  : 100},
+                  {text : '精确度', max  : 100},
+                  {text : '持久力', max  : 100},
+                  {text : '射程', max  : 100}
+              ],
+              radius : 130
+          }
+      ],
+      series : [
+          {
+              name: '替身数据',
+              type: 'radar',
+              itemStyle: {
+                  normal: {
+                    color:'#87CEEB',
+                      areaStyle: {
+                          type: 'default'
+                      }
+                  }
+              },
+              data : [
+                  {
+                      value : [this.map[hero.strength],
+                               this.map[hero.speed], 
+                               this.map[hero.growth], 
+                               this.map[hero.precise], 
+                               this.map[hero.persistentce], 
+                               this.map[hero.distance]]
+                  }
+              ]
+          }
+      ]
+  };
+  }
+
+  charToNumber() : void
+  {
+    this.map["A"] = 100;
+    this.map["B"] = 80;
+    this.map["C"] = 60;
+    this.map["D"] = 40;
+    this.map["E"] = 20;
+    
+  } 
 }
